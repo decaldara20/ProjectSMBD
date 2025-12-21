d537188db16ef2048c7c4ddcafa41eb6409b838e
trishagarniss <trishagarniss@gmail.com>
2025-12-21 11:48:50 +0700

    add: route POST register

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,8 @@ Route::get('/api/search/suggestions', [GuestController::class, 'getSuggestions']
 // Route Autentikasi Custom
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 // Route Logout (Wajib POST demi keamanan)
